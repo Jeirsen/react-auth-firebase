caebd74bca79a32206a67f4fffc3fa45d6971418
Jeirsen <jeirsen@gmail.com>
2019-05-06 23:04:13 -0500

    added redux and react-redux

---
+++ src/store/actions/projectActions.js
@@ -0,0 +1,9 @@
+export const createProject = (project) => {
+    return (dispatch, getState) => {
+        // make async code to database 
+        dispatch({
+            type: 'CREATE_PROJECT',
+            project
+        })
+    }
+}
